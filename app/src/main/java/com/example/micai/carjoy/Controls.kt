68809b9fc944f1737ea5dbb59843d83272040b78
mishon-nippon <mik43182273@yandex.ru>
2018-05-15 14:46:25 +0300

    updates
    Added connection activity and rebuild control activity. Changed the format of send data

---
+++ app/src/main/java/com/example/micai/carjoy/Controls.kt
@@ -0,0 +1,156 @@
+package com.example.micai.carjoy
+
+import android.app.ProgressDialog
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
+import android.content.Context
+import android.content.Intent
+import android.os.AsyncTask
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.controls_activity.*
+import kotlinx.android.synthetic.main.controls_activity.view.*
+import org.jetbrains.anko.toast
+import java.io.IOException
+import java.util.*
+
+
+
+class ControlsActivity : AppCompatActivity() {
+    companion object {
+        lateinit var MacAddress: String
+        var MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+        lateinit var m_bluetoothAdapter: BluetoothAdapter
+        lateinit var m_progress: ProgressDialog
+        var m_bluetoothSocket: BluetoothSocket? = null
+        var m_Connected: Boolean = false
+    }
+
+    private var motorLeft = 0
+    private var motorRight = 0
+    private var pwmBtnMotorLeft: Int = 0
+    private var pwmBtnMotorRight: Int = 0
+    private var commandLeft: String? = null
+    private var commandRight: String? = null
+
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.controls_activity)
+
+        MacAddress = intent.getStringExtra(MainActivity.MAC_ADRESS)
+
+        ConnectToDevice(this).execute()
+
+        this.pwmBtnMotorLeft = Integer.parseInt(resources.getText(R.string.default_pwmBtnMotorLeft) as String)
+        this.pwmBtnMotorRight = Integer.parseInt(resources.getText(R.string.default_pwmBtnMotorRight) as String)
+        this.commandLeft = resources.getText(R.string.default_commandLeft) as String
+        this.commandRight = resources.getText(R.string.default_commandRight) as String
+
+
+        button_for.setOnClickListener {
+            motorLeft = -pwmBtnMotorLeft
+            motorRight = -pwmBtnMotorRight
+            sendData(commandLeft + motorLeft + "\r" + commandRight + motorRight + "\r")
+
+        }
+
+        button_back.setOnClickListener {
+            motorLeft = -pwmBtnMotorLeft
+            motorRight = -pwmBtnMotorRight
+            sendData(commandLeft + motorLeft + "\r" + commandRight + motorRight + "\r")
+        }
+
+        button_left.setOnClickListener {
+            motorLeft = 0
+            motorRight = pwmBtnMotorRight
+            sendData(commandLeft + motorLeft + "\r" + commandRight + motorRight + "\r")
+        }
+
+        button_right.setOnClickListener {
+            motorLeft = pwmBtnMotorLeft
+            motorRight = 0
+            sendData(commandLeft + motorLeft + "\r" + commandRight + motorRight + "\r")
+        }
+
+
+    }
+
+
+    fun sendData(input: String) {
+        if (m_bluetoothSocket != null) { try {
+            m_bluetoothSocket!!.outputStream.write(input.toByteArray())
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        }
+    }
+
+    fun disconnect() {
+        if (m_bluetoothSocket != null) {
+            try {
+                m_bluetoothSocket!!.close()
+                m_bluetoothSocket = null
+                m_Connected = false
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+        finish()
+    }
+
+    private class ConnectToDevice(c: Context) : AsyncTask<Void, Void, String>() {
+        private var connectSucess: Boolean = true
+        private val context: Context
+        init {
+            this.context = c
+        }
+        override fun onPreExecute() {
+            super.onPreExecute()
+            m_progress = ProgressDialog.show(context, "Соединение", "Пожалуйста, подождите")
+        }
+
+        override fun doInBackground(vararg params: Void?): String? {
+            try {
+                if (m_bluetoothSocket == null || m_Connected) {
+                    m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+                    val device: BluetoothDevice = m_bluetoothAdapter.getRemoteDevice(MacAddress)
+                    m_bluetoothSocket = device.createInsecureRfcommSocketToServiceRecord(MY_UUID)
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery()
+                    m_bluetoothSocket!!.connect()
+                }
+            } catch (e: IOException) {
+                connectSucess = false
+                e.printStackTrace()
+            }
+            return null
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            if (!connectSucess) {
+                Log.i("data", "Не могу соедениться")
+            } else {
+                m_Connected = true
+            }
+            m_progress.dismiss()
+        }
+
+
+    }
+
+
+}
+
+
+
+
--- app/src/main/java/com/example/micai/carjoy/MainActivity.kt
+++ app/src/main/java/com/example/micai/carjoy/MainActivity.kt
@@ -1,12 +1,85 @@
 package com.example.micai.carjoy
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.main_activity.*
+import org.jetbrains.anko.toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+
+    private var m_bluetoothAdapter: BluetoothAdapter?=null
+    private lateinit var m_pairedDevices: Set<BluetoothDevice>
+    private val REQUEST_ENABLE_BLUETOOTH = 1
+
+    companion object {
+        val MAC_ADRESS: String = "Device_adress"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_activity)
+
+        m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+
+        if (m_bluetoothAdapter == null) {
+            toast("Bluetooth не поддерживается")
+
+        } else if (!m_bluetoothAdapter!!.isEnabled) {
+            val btIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(btIntent, REQUEST_ENABLE_BLUETOOTH)
+        }
+
+    select_device_refresh.setOnClickListener{ pairedDeviceList()}
+
+
+    }
+
+
+    private fun pairedDeviceList() {
+        m_pairedDevices = m_bluetoothAdapter!!.bondedDevices
+        val list: ArrayList<BluetoothDevice> = ArrayList()
+
+        if (!m_pairedDevices.isEmpty()) {
+            for (device: BluetoothDevice in m_pairedDevices) {
+                list.add(device)
+                Log.i("device", "" + device)
+            }
+        } else {
+            toast("Устройств не найдено")
+        }
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+        select_device_list.adapter = adapter
+        select_device_list.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            val device: BluetoothDevice = list[position]
+            val address: String = device.address
+
+            val intent = Intent(this, ControlsActivity::class.java)
+            intent.putExtra(MAC_ADRESS, address)
+            startActivity(intent)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_ENABLE_BLUETOOTH){
+            if (resultCode == Activity.RESULT_OK){
+                if (m_bluetoothAdapter!!.isEnabled){
+                    toast("Bluetooth включен")
+                }else {
+                    toast("Bluetooth выключен")
+                }
+            }else if (resultCode == Activity.RESULT_CANCELED){
+                toast("Активация bluetooth была закрыта")
+            }
+        }
     }
 }
